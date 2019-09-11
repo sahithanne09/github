91e83126e322871e920e1c184c10d74d49e416df
sahithanne09 <55160949+sahithanne09@users.noreply.github.com>
2019-09-11 18:00:30 +0530

    Add files via upload

---
+++ capstone_coursera.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# The Notebook is used for capstone
+# 
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
